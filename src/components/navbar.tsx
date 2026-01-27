8f0661e0d83cdbb5befbfba36dbcad6f844291f6
Antoni Saputra <antonisaputra049@gmail.com>
2026-01-27 22:36:30 +0700

    feat: add link to repository

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,10 +20,14 @@ function Navbar() {
   return (
     <nav className="border-b h-16 flex justify-between items-center px-15 fixed top-0 left-0 w-full z-0 bg-secondary">
       <h1 className="font-semibold">excel-opener</h1>
-      <div className="flex items-center gap-2">
+      <a
+        href="https://github.com/luxuling/excel-opener"
+        target="_blank"
+        className="flex items-center gap-2"
+      >
         <DinkieIconsGithub width={16} height={16} />
         <p className="text-sm font-medium">{formatStars(stars ?? 0)}</p>
-      </div>
+      </a>
     </nav>
   )
 }
